b8a8124e39b0b444abb4f311933b829c20f3898c
sifad-hossain <niaz.sifad@gmail.com>
2024-06-11 16:14:26 +0600

    some code edit

--- index.js
+++ index.js
@@ -3,227 +3,292 @@ const app = express();
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
+const cookieParser = require('cookie-parser')
 const port = process.env.PORT || 4000;
 
 
-//middlewares
-app.use(cors())
-app.use(express.json())
+// middleware
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 
+app.use(express.json())
+app.use(cookieParser())
+
+// Verify Token Middleware
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
+  console.log('her token', token)
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log('error', err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dh7dofl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 async function run() {
-    try {
-        const userCollection = client.db("techHiveDB").collection("users")
-        const productCollection = client.db("techHiveDB").collection("products")
-        const reviewCollection = client.db('techHiveDB').collection('review')
-        //jwt related api
-        app.post('/jwt', async (req, res) => {
-            const user = req.body;
-            /**
-             * const token = sign(payload: string | object | Buffer, secretOrPrivateKey: jwt.Secret, options?: jwt.SignOptions | undefined): string (+4 overloads)
-            Synchronously sign the given payload into a JSON Web Token string payload - Payload to sign, could be an literal, buffer or string secretOrPrivateKey - Either the secret for HMAC algorithms, or the PEM encoded private key for RSA and ECDSA. [options] - Options for the signature returns - The JSON Web Token string
-             */
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,
-                { expiresIn: '24h' })
-            /**Eikhane second braket er modde keno token ta dice */
-            res.send({ token })
-        })
-
-        //user related api
-        app.post('/users', async (req, res) => {
-            const user = req.body;
-
-            const query = { email: user.email }
-            const existingUser = await userCollection.findOne(query);
-            if (existingUser) {
-                return res.send({ message: 'user already exists', insertedId: null })
-            }
-
-            const result = await userCollection.insertOne(user)
-            res.send(result)
-        })
-
-        //Middlewares
-        const verifyToken = (req, res, next) => {
-            console.log('inside', req.headers.authorization);
-            if (!req.headers.authorization) {
-                return res.status(401).send({ message: 'Unauthorized access' })
-            }
-            const token = req.headers.authorization.split(' ')[1];
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-                if (err) {
-                    return res.status(401).send({ message: 'Unauthorized access' })
-                }
-                req.decoded = decoded;
-                next()
-            })
-        }
-
-        //use verify admin after verifyToken
-        const verifyAdmin = async (req, res, next) => {
-            const email = req.decoded.email;
-            const query = { email: email }
-            const user = await userCollection.findOne(query)
-            const isAdmin = user?.role === 'admin';
-            if (!isAdmin) {
-                return res.status(403).send({ message: 'forbidden access' })
-            }
-            next()
-        }
-
-
-        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
-            const result = await userCollection.find().toArray()
-            res.send(result)
-        })
-
-
-        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) }
-            const updateDoc = {
-                $set: {
-                    role: 'admin',
-                }
-            }
-            const result = await userCollection.updateOne(filter, updateDoc)
-            res.send(result)
-        })
-
-        app.post('/products', async (req, res) => {
-            const newProduct = req.body
-            const result = await productCollection.insertOne(newProduct)
-            res.send(result)
-        })
-
-        app.get('/products', async (req, res) => {
-            const result = await productCollection.find().toArray()
-            res.send(result)
-        })
-
-
-        app.get('/users/admin/:email', verifyToken, async (req, res) => {
-            const email = req.params.email;
-            if (email !== req.decoded.email) {
-                return res.status(403).send({ message: 'forbidden access' })
-            }
-            const query = { email: email }
-            const user = await userCollection.findOne(query)
-            let admin = false;
-            if (user) {
-                admin = user?.role === 'admin';
-            }
-            res.send({ admin })
-        })
-
-
-        /**Conceptual sessino task:  */
-
-        app.get('/user/:email', async (req, res) => {
-            const email = req.params.email;
-            const query = { email: email }
-            const result = await userCollection.findOne(query)
-            res.send(result)
-        })
+  try {
+    const usersCollection = client.db("techHiveDB").collection("users")
+    const productCollection = client.db("techHiveDB").collection("products")
+    const reviewCollection = client.db('techHiveDB').collection('review')
 
 
 
+    //1. verify admin middleware
+    const verifyAdmin = async (req, res, next) => {
+      console.log('hello')
+      const user = req.user
+      const query = { email: user?.email }
+      const result = await usersCollection.findOne(query)
+      console.log(result?.role)
+      if (!result || result?.role !== 'admin')
+        return res.status(401).send({ message: 'unauthorized access!!' })
 
-
-
-
-
-        app.delete('/products/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await productCollection.deleteOne(query)
-            res.send(result)
-        })
-
-        app.get('/products/id/:id', async (req, res) => {
-            const id = req.params.id
-            const result = await productCollection.findOne({ _id: new ObjectId(id) })
-            res.send(result)
-        })
-
-        app.get('/products/:email', async (req, res) => {
-
-            const email = req.params.email
-            console.log(email);
-            const result = await productCollection.find({ user_email: email }).toArray()
-            console.log(result);
-            res.send(result)
-        })
-
-
-
-        // Update product vote
-        app.patch('/products/vote/:id', async (req, res) => {
-            const id = req.params.id
-            let upVote = req.body.upvote
-            let voterId = req.body.voterId
-            let downVote = req.body.downvote
-            let status = req.body.status
-            if (status) {
-                upVote?.push(voterId)
-            } else {
-                downVote?.push(voterId)
-            }
-            console.log(status)
-            console.log({ upVote, downVote })
-            // const query = { _id: id }
-            const query = { _id: new ObjectId(id) }
-            const updateDoc = {
-                $set: {
-                    upVote,
-                    downVote,
-                },
-            }
-            const result = await productCollection.updateOne(query, updateDoc)
-            res.send(result)
-        })
-
-        app.post('/addedreview', async (req, res) => {
-            const review = req.body
-            const result = await reviewCollection.insertOne(review)
-            res.send(result)
-        })
-
-        app.get('/addedreview', async (req, res) => {
-            const result = await reviewCollection.find().toArray()
-            res.send(result)
-        })
-
-        // Send a ping to confirm a successful connection
-        await client.db('admin').command({ ping: 1 })
-        console.log(
-            'Pinged your deployment. You successfully connected to MongoDB!'
-        )
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+      next()
     }
+
+
+    // For Moderators
+    const verifyModerator = async (req, res, next) => {
+      const user = req.user
+      const query = { email: user?.email }
+      const result = await usersCollection.findOne(query)
+      if (!result || result?.role !== 'moderator')
+        return res.status(401).send({ message: 'unauthorized access' })
+      next()
+    }
+
+    //3. auth related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res.send({token})
+      
+    })
+    // Logout
+    app.get('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+        console.log('Logout successful')
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
+
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+
+    //4. save a user data in db
+    app.put('/user', async (req, res) => {
+      const user = req.body
+
+      const query = { email: user?.email }
+      // check if user already exists in db
+      const isExist = await usersCollection.findOne(query)
+      if (isExist) {
+        if (user.status === 'Requested') {
+          // if existing user try to change his role
+          const result = await usersCollection.updateOne(query, {
+            $set: { status: user?.status },
+          })
+          return res.send(result)
+        } else {
+          // if existing user login again
+          return res.send(isExist)
+        }
+      }
+
+      //5. save user for the first time
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now(),
+        },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc, options)
+      res.send(result)
+    })
+
+      //7. get all users data from db
+      app.get('/users',  async (req, res) => {
+        const result = await usersCollection.find().toArray()
+        res.send(result)
+      })
+  
+
+    //6. get a user info by email from db
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email
+      const result = await usersCollection.findOne({ email })
+      res.send(result)
+    })
+
+
+  
+
+    //8.update a user role
+    app.patch('/users/update/:email', async (req, res) => {
+      const email = req.params.email
+      const user = req.body
+      const query = { email }
+      const updateDoc = {
+        $set: { ...user, timestamp: Date.now() },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    app.post('/products', async (req, res) => {
+      const newProduct = req.body
+      const result = await productCollection.insertOne(newProduct)
+      res.send(result)
+    })
+
+    app.get('/products', async (req, res) => {
+      const result = await productCollection.find().toArray()
+      res.send(result)
+    })
+
+
+
+
+
+    app.delete('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await productCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.get('/products/id/:id', async (req, res) => {
+      const id = req.params.id
+      const result = await productCollection.findOne({ _id: new ObjectId(id) })
+      res.send(result)
+    })
+
+    app.get('/products/:email', async (req, res) => {
+
+      const email = req.params.email
+      console.log(email);
+      const result = await productCollection.find({ user_email: email }).toArray()
+      console.log(result);
+      res.send(result)
+    })
+
+
+
+    // Update product vote
+    app.patch('/products/vote/:id', async (req, res) => {
+      const id = req.params.id
+      let upVote = req.body.upvote
+      let voterId = req.body.voterId
+      let downVote = req.body.downvote
+      let status = req.body.status
+      if (status) {
+        upVote?.push(voterId)
+      } else {
+        downVote?.push(voterId)
+      }
+      console.log(status)
+      console.log({ upVote, downVote })
+      // const query = { _id: id }
+      const query = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          upVote,
+          downVote,
+        },
+      }
+      const result = await productCollection.updateOne(query, updateDoc)
+      res.send(result)
+    })
+
+    app.post('/addedreview', async (req, res) => {
+      const review = req.body
+      const result = await reviewCollection.insertOne(review)
+      res.send(result)
+    })
+
+    app.get('/addedreview', async (req, res) => {
+      const result = await reviewCollection.find().toArray()
+      res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
 
 
 app.get('/', (req, res) => {
-    res.send('techHive your new distgnation')
+  res.send('techHive your new distgnation')
 })
 
 app.listen(port, (req, res) => {
-    console.log(`TechHive your server side, ${port}`);
+  console.log(`TechHive your server side, ${port}`);
 })
